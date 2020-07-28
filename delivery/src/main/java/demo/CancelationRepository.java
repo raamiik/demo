4aaaff483d4e7ae98eb0624a1c41ca8f2ad271b4
raamiik <warm.wiind@gmail.com>
2020-07-28 15:27:11 +0900

    1st commit

---
+++ delivery/src/main/java/demo/CancelationRepository.java
@@ -0,0 +1,8 @@
+package demo;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CancelationRepository extends PagingAndSortingRepository<Cancelation, Long>{
+
+
+}
